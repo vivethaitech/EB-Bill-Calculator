690a45729296543f9eff4a0974bead8a232dd369
Vivetha Baskaran <vivethaitech@gmail.com>
2024-07-21 23:04:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -51,13 +51,21 @@ function calculateMonthlyBill(usage) {
 function calculateBillForUnits(units) {
     let billAmount = 0;
     if (units <= 100) {
-        billAmount = units * 1.5;
-    } else if (units <= 200) {
-        billAmount = (100 * 1.5) + ((units - 100) * 2.5);
-    } else if (units <= 300) {
-        billAmount = (100 * 1.5) + (100 * 2.5) + ((units - 200) * 4);
-    } else {
-        billAmount = (100 * 1.5) + (100 * 2.5) + (100 * 4) + ((units - 300) * 6);
+    billAmount = units * 0;
+    } else if (units>100 && units <= 200) {
+    billAmount = (100 * 0) + ((units - 100) * 2.25);
+    } else if (units>200 && units <= 400) {
+    billAmount = (100 * 0) + (100 * 2.25) + ((units - 200) * 4.50);
+    } else if(units>400 && units <= 500){
+    billAmount = (100 * 0) + (100 * 2.25) + (200 * 4.50) + ((units - 400) * 6.00);
+    } else if(units>500 && units <= 600){
+    billAmount = (100 * 0) + (300 * 4.50) + (100 * 6.00)+((units - 500) * 8.00);
+    } else if(units>600 && units <= 800){
+    billAmount = (100 * 0) + (300 * 4.50) + (100 * 6.00)+ (100 * 8.00)+ ((units - 600) * 9.00);
+    } else if(units>800 && units<=1000){
+    billAmount = (100 * 0) + (300 * 4.50) + (100 * 6.00)+ (100 * 8.00)+ (200 * 9.00) + ((units - 800) * 10.00);
+    } else if(units>1000){
+    billAmount = (100 * 0) + (300 * 4.50) + (100 * 6.00)+ (100 * 8.00)+ (200 * 9.00) + (200 * 10.00) + ((units - 1000) * 11.00);
     }
     return billAmount;
 }
