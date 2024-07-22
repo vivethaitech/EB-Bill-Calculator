331bbfb739b855e864038289f414ef148be82ec5
Vivetha Baskaran <vivethaitech@gmail.com>
2024-07-22 23:35:03 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,55 +1,7 @@
-document.getElementById('eb-form').addEventListener('submit', function(event) {
-    event.preventDefault(); // Prevent form submission
-    calculateBill();
-});
-
 function calculateBill() {
-    const salary = parseFloat(document.getElementById('salary').value);
-    const usage = [];
-    for (let i = 1; i <= 6; i++) {
-        const usageValue = parseFloat(document.getElementById(`month${i}`).value);
-        if (!isNaN(usageValue)) {
-            usage.push(usageValue);
-        } else {
-            usage.push(0); // Push 0 if the input is empty or invalid
-        }
-    }
-
-    // Use the provided unit-based calculation
-    const biMonthlyBill = calculateBiMonthlyBill(usage);
-    const monthlyBill = calculateMonthlyBill(usage);
-    const savings = monthlyBill - biMonthlyBill;
-    const percentageSavings = (savings / monthlyBill) * 100;
-
-    const results = document.getElementById('results');
-    results.innerHTML = `
-        <h4>BiMonthly bill: ₹${biMonthlyBill.toFixed(2)}</h4>
-        <h4>Monthly bill: ₹${monthlyBill.toFixed(2)}</h4>
-        <h4>Savings(if monthly bill): ₹${savings.toFixed(2)} and saving percentage(if monthly bill): ${percentageSavings.toFixed(2)}% </h4>
-        <h4>percentage of salary for BiMonthly payment: ${(biMonthlyBill / salary * 100).toFixed(2)}%</h4>
-        <h4>percentage of salary for Monthly payment: ${(monthlyBill / salary * 100).toFixed(2)}%</h4>
-    `;
-}
-
-function calculateBiMonthlyBill(usage) {
-    let totalBill = 0;
-    for (let i = 0; i < usage.length; i += 2) {
-        const biMonthlyUsage = usage[i] + (usage[i + 1] || 0);
-        totalBill += calculateBillForUnits(biMonthlyUsage);
-    }
-    return totalBill;
-}
-
-function calculateMonthlyBill(usage) {
-    let totalBill = 0;
-    for (const monthlyUsage of usage) {
-        totalBill += calculateBillForUnits(monthlyUsage);
-    }
-    return totalBill;
-}
-
-function calculateBillForUnits(units) {
+    const units = document.getElementById('units').value;
     let billAmount = 0;
+    
     if (units <= 100) {
     billAmount = units * 0;
     } else if (units>100 && units <= 200) {
@@ -67,5 +19,7 @@ function calculateBillForUnits(units) {
     } else if(units>1000){
     billAmount = (100 * 0) + (300 * 4.50) + (100 * 6.00)+ (100 * 8.00)+ (200 * 9.00) + (200 * 10.00) + ((units - 1000) * 11.00);
     }
-    return billAmount;
-}
+    
+    document.getElementById('result').innerText = `Total Bill: ₹${billAmount.toFixed(2)}`;
+    }
+    
